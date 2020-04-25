b3a3ce2bd2a2d034ab16fac058595230b4005619
Thomas Kluge <th.kluge@me.com>
2020-04-25 20:40:38 +0200

    added first test

---
+++ test/001_test_contact.js
@@ -0,0 +1,95 @@
+const assert = require('assert')
+const path = require('path')
+const Logger = require(path.join(__dirname, '..', 'lib', 'logger.js'))
+const Server = require(path.join(__dirname, '..', 'lib', 'Server.js'))
+const Service = require('hap-nodejs').Service
+const Characteristic = require('hap-nodejs').Characteristic
+const expect = require('expect.js')
+
+const fs = require('fs')
+let log = new Logger('HAP Test')
+log.setDebugEnabled(false)
+
+describe('HAP-Homematic Tests', () => {
+  let that = this
+
+  before(async () => {
+    log.info('preparing tests')
+    let datapath = path.join(__dirname, 'devices', 'HmIP-SWDM.json')
+    let strData = fs.readFileSync(datapath).toString()
+    if (strData) {
+      that.data = JSON.parse(strData)
+
+      that.server = new Server(log)
+
+      await that.server.simulate(undefined, {config: {
+        channels: Object.keys(that.data.ccu)
+      },
+      devices: that.data.devices})
+    } else {
+      assert.ok(false, 'Unable to load Test data')
+    }
+  })
+
+  after(() => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      accessory.shutdown()
+    })
+  })
+
+  it('HAP-Homematic check test mode', (done) => {
+    expect(that.server.isTestMode).to.be(true)
+    done()
+  })
+
+  it('HAP-Homematic check number of ccu devices', (done) => {
+    expect(that.server._ccu.getCCUDevices().length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check number of mappend devices', (done) => {
+    expect(Object.keys(that.server._publishedAccessories).length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check assigned services', (done) => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      expect(accessory.serviceClass).to.be(that.data.ccu[accessory.address()])
+    })
+    done()
+  })
+
+  it('HAP-Homematic check STATE 0', (done) => {
+    that.server._ccu.fireEvent('HmIP.0123456789ABCD:1.STATE', false)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.ContactSensor)
+    assert.ok(service, 'Contact Service not found')
+    let ch = service.getCharacteristic(Characteristic.ContactSensorState)
+    assert.ok(ch, 'Contact State Characteristics not found')
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(0)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+
+  it('HAP-Homematic check STATE 1', (done) => {
+    that.server._ccu.fireEvent('HmIP.0123456789ABCD:1.STATE', true)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.ContactSensor)
+    let ch = service.getCharacteristic(Characteristic.ContactSensorState)
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(1)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+})
