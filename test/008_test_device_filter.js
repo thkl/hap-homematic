b2037473279e026b73cf2646637946d989558a6e
Thomas Kluge <th.kluge@me.com>
2020-05-05 19:28:07 +0200

    added a test for device filters and we have another KEY_TRANSCEIVER service to apply this filter .. thanks to the tests

---
+++ test/008_test_device_filter.js
@@ -0,0 +1,60 @@
+const assert = require('assert')
+const path = require('path')
+const Logger = require(path.join(__dirname, '..', 'lib', 'logger.js'))
+const Server = require(path.join(__dirname, '..', 'lib', 'Server.js'))
+const expect = require('expect.js')
+
+const fs = require('fs')
+let log = new Logger('HAP Test')
+log.setDebugEnabled(false)
+
+const testCase = 'HmIP-ASIR-2.json'
+
+describe('HAP-Homematic Tests ' + testCase, () => {
+  let that = this
+
+  before(async () => {
+    log.debug('preparing tests')
+    let datapath = path.join(__dirname, 'devices', testCase)
+    let strData = fs.readFileSync(datapath).toString()
+    if (strData) {
+      that.data = JSON.parse(strData)
+
+      that.server = new Server(log)
+
+      await that.server.simulate(undefined, {config: {
+        channels: Object.keys(that.data.ccu)
+      },
+      devices: that.data.devices})
+    } else {
+      assert.ok(false, 'Unable to load Test data')
+    }
+  })
+
+  after(() => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      accessory.shutdown()
+    })
+  })
+
+  it('HAP-Homematic check test mode', (done) => {
+    expect(that.server.isTestMode).to.be(true)
+    done()
+  })
+
+  it('HAP-Homematic check number of ccu devices', (done) => {
+    expect(that.server._ccu.getCCUDevices().length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check number of mappend devices should be 0 cause of filters', (done) => {
+    expect(Object.keys(that.server._publishedAccessories).length).to.be(0)
+    done()
+  })
+
+  it('HAP-Homematic check number of compatible devices should be also 0 cause of filters', (done) => {
+    expect(Object.keys(that.server._compatibleDevices).length).to.be(0)
+    done()
+  })
+})
