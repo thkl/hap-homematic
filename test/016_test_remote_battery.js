a155620a78afcf9b3fafeb67be686505244fe3c2
Thomas Kluge <th.kluge@me.com>
2020-05-25 19:53:48 +0200

    added battery levvel  (Issue #90)

---
+++ test/016_test_remote_battery.js
@@ -0,0 +1,116 @@
+const assert = require('assert')
+const path = require('path')
+const Logger = require(path.join(__dirname, '..', 'lib', 'logger.js'))
+const Server = require(path.join(__dirname, '..', 'lib', 'Server.js'))
+const Service = require('hap-nodejs').Service
+const Characteristic = require('hap-nodejs').Characteristic
+const expect = require('expect.js')
+
+const fs = require('fs')
+let log = new Logger('HAP Test')
+log.setDebugEnabled(false)
+
+const testCase = 'HmIP-KRCA.json'
+
+describe('HAP-Homematic Tests ' + testCase, () => {
+  let that = this
+
+  before(async () => {
+    log.debug('preparing tests')
+    let datapath = path.join(__dirname, 'devices', testCase)
+    let strData = fs.readFileSync(datapath).toString()
+    if (strData) {
+      that.data = JSON.parse(strData)
+
+      that.server = new Server(log)
+
+      await that.server.simulate(undefined, {config: {
+        channels: Object.keys(that.data.ccu)
+      },
+      devices: that.data.devices,
+      mappings: that.data.mappings,
+      values: { // add dummy values so hazDatapoint will find this DP and the device will get HMIP Style battery checks
+        'HmIP.4436784678ABCD:0.LOW_BAT': false,
+        'HmIP.4436784678ABCD:0.OPERATING_VOLTAGE': 0
+      }
+      })
+    } else {
+      assert.ok(false, 'Unable to load Test data')
+    }
+  })
+
+  after(() => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      accessory.shutdown()
+    })
+  })
+
+  it('HAP-Homematic check test mode', (done) => {
+    expect(that.server.isTestMode).to.be(true)
+    done()
+  })
+
+  it('HAP-Homematic check number of ccu devices', (done) => {
+    expect(that.server._ccu.getCCUDevices().length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check number of mappend devices', (done) => {
+    expect(Object.keys(that.server._publishedAccessories).length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check assigned services', (done) => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      expect(accessory.serviceClass).to.be(that.data.ccu[accessory.address()])
+    })
+    done()
+  })
+
+  it('HAP-Homematic test low bat', (done) => {
+    that.server._ccu.fireEvent('HmIP.4436784678ABCD:0.LOW_BAT', true)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.BatteryService)
+    let ch = service.getCharacteristic(Characteristic.StatusLowBattery)
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+
+  it('HAP-Homematic test low bat negative', (done) => {
+    that.server._ccu.fireEvent('HmIP.4436784678ABCD:0.LOW_BAT', false)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.BatteryService)
+    let ch = service.getCharacteristic(Characteristic.StatusLowBattery)
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+
+  it('HAP-Homematic test voltage reading 0.6V 50%', (done) => {
+    that.server._ccu.fireEvent('HmIP.4436784678ABCD:0.OPERATING_VOLTAGE', 0.6)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.BatteryService)
+    let ch = service.getCharacteristic(Characteristic.BatteryLevel)
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(50)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+})
