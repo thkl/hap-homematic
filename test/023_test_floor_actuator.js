d414b4d4d6e09c73f3b9bb94022565ad9f18f452
Thomas Kluge <th.kluge@me.com>
2020-06-16 20:34:41 +0200

    added tests for floor heating actuator (#128)

---
+++ test/023_test_floor_actuator.js
@@ -0,0 +1,89 @@
+const assert = require('assert')
+const path = require('path')
+const Logger = require(path.join(__dirname, '..', 'lib', 'logger.js'))
+const Server = require(path.join(__dirname, '..', 'lib', 'Server.js'))
+const expect = require('expect.js')
+const EveHomeKitValveTypes = require(path.join(__dirname, '..', 'lib', 'services', 'EveValve.js'))
+
+const fs = require('fs')
+let log = new Logger('HAP Test')
+log.setDebugEnabled(false)
+
+const testCase = 'HmIP-FALMOT-C12.json'
+
+describe('HAP-Homematic Tests ' + testCase, () => {
+  let that = this
+
+  before(async () => {
+    log.debug('preparing tests')
+    let datapath = path.join(__dirname, 'devices', testCase)
+    let strData = fs.readFileSync(datapath).toString()
+    if (strData) {
+      that.data = JSON.parse(strData)
+
+      that.server = new Server(log)
+
+      await that.server.simulate(undefined, {config: {
+        channels: Object.keys(that.data.ccu)
+      },
+      devices: that.data.devices,
+      mappings: that.data.mappings,
+      values: {
+        'HmIP.4664078465ABCD:2.LEVEL': 0
+      }
+      })
+    } else {
+      assert.ok(false, 'Unable to load Test data')
+    }
+  })
+
+  after(() => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      accessory.shutdown()
+    })
+  })
+
+  it('HAP-Homematic check test mode', (done) => {
+    expect(that.server.isTestMode).to.be(true)
+    done()
+  })
+
+  it('HAP-Homematic check number of ccu devices', (done) => {
+    expect(that.server._ccu.getCCUDevices().length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check number of mappend devices', (done) => {
+    expect(Object.keys(that.server._publishedAccessories).length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check assigned services', (done) => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      expect(accessory.serviceClass).to.be(that.data.ccu[accessory.address()])
+    })
+    done()
+  })
+
+  it('HAP-Homematic check Valve with random value', (done) => {
+    let rnd = Math.floor(Math.random() * Math.floor(99)) / 100
+    that.server._ccu.fireEvent('HmIP.4664078465ABCD:2.LEVEL', rnd)
+    let eveValve = new EveHomeKitValveTypes(that.server.gatoHomeBridge.hap)
+
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(eveValve.Service.ValveService)
+    assert.ok(service, 'ValveService Service not found')
+    let ch = service.getCharacteristic(eveValve.Characteristic.CurrentValveState)
+    assert.ok(ch, 'CurrentValveState State Characteristics not found')
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be((rnd * 100))
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+})
