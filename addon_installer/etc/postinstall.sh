d1e258bfd6ce2b99d09af8ddfe37f68020210fbe
Thomas Kluge <th.kluge@me.com>
2020-10-05 17:22:41 +0200

    changed location of post installer so it will be in the backup and postinstall can run after a ccu restore

--- addon_installer/etc/postinstall.sh
+++ addon_installer/etc/postinstall.sh
@@ -7,6 +7,7 @@ ADDONWWW_DIR=/usr/local/etc/config/addons/www
 NPMCACHE_DIR=/tmp/hap-homematic-cache
 RCD_DIR=${CONFIG_DIR}/rc.d
 LOGFILE=/var/log/hmhapinstall.log
+echo "[Installer]Check existency of the daemon" >>${LOGFILE}
 
 #check if we have our core module; if not go ahead and call the installer stuff
 if [ ! -f ${ADDON_DIR}/node_modules/hap-homematic/index.js ]; then
@@ -27,5 +28,6 @@ echo "[Installer]creating HomeKit Button ...">>${LOGFILE}
 node ${ADDON_DIR}/node_modules/${ADDONNAME}/etc/hm_addon.js hap ${ADDON_DIR}/node_modules/${ADDONNAME}/etc/hap_addon.cfg
 #create the .nobackup file into the plugin directory to prevent backing up all the node depencities
 touch ${ADDON_DIR}/.nobackup
-
+else
+echo "[Installer]daemon exists lets light this candle" >>${LOGFILE}
 fi
