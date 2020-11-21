49453f487874033ea2f3480cf033bc805997b53c
Thomas Kluge <th.kluge@me.com>
2020-11-21 14:53:28 +0100

    added HmIP-STD to 2 Services (#252)

---
+++ test/032_test_thermometer_sth.js
@@ -0,0 +1,104 @@
+const assert = require('assert')
+const path = require('path')
+const Logger = require(path.join(__dirname, '..', 'lib', 'logger.js'))
+const Server = require(path.join(__dirname, '..', 'lib', 'Server.js'))
+const Service = require('hap-nodejs').Service
+const Characteristic = require('hap-nodejs').Characteristic
+const expect = require('expect.js')
+
+const fs = require('fs')
+let log = new Logger('HAP Test')
+log.setDebugEnabled(false)
+
+const testCase = 'HmIP-STH.json'
+
+describe('HAP-Homematic Tests ' + testCase, () => {
+  let that = this
+
+  before(async () => {
+    log.debug('preparing tests')
+    let datapath = path.join(__dirname, 'devices', testCase)
+    let strData = fs.readFileSync(datapath).toString()
+    if (strData) {
+      that.data = JSON.parse(strData)
+
+      that.server = new Server(log)
+
+      await that.server.simulate(undefined, {config: {
+        channels: Object.keys(that.data.ccu)
+      },
+      devices: that.data.devices,
+      mappings: that.data.mappings,
+      values: {
+        'HmIP.4734919797ABCD:1.HUMIDITY': 1
+      }
+      })
+    } else {
+      assert.ok(false, 'Unable to load Test data')
+    }
+  })
+
+  after(() => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      accessory.shutdown()
+    })
+  })
+
+  it('HAP-Homematic check test mode', (done) => {
+    expect(that.server.isTestMode).to.be(true)
+    done()
+  })
+
+  it('HAP-Homematic check number of ccu devices', (done) => {
+    expect(that.server._ccu.getCCUDevices().length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check number of mappend devices', (done) => {
+    expect(Object.keys(that.server._publishedAccessories).length).to.be(1)
+    done()
+  })
+
+  it('HAP-Homematic check assigned services', (done) => {
+    Object.keys(that.server._publishedAccessories).map(key => {
+      let accessory = that.server._publishedAccessories[key]
+      expect(accessory.serviceClass).to.be(that.data.ccu[accessory.address()])
+    })
+    done()
+  })
+
+  it('HAP-Homematic check ACTUAL_TEMPERATURE with random value', (done) => {
+    let rnd = Math.floor(Math.random() * Math.floor(30))
+    that.server._ccu.fireEvent('HmIP.4734919797ABCD:1.ACTUAL_TEMPERATURE', rnd)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.TemperatureSensor, 'TestDevice', false, '', true)
+    assert.ok(service, 'Thermostat Service not found')
+    let ch = service.getCharacteristic(Characteristic.CurrentTemperature)
+    assert.ok(ch, 'CurrentTemperature State Characteristics not found')
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(rnd)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+
+  it('HAP-Homematic check HUMIDITY with random value', (done) => {
+    let rnd = Math.floor(Math.random() * Math.floor(100))
+    that.server._ccu.fireEvent('HmIP.4734919797ABCD:1.HUMIDITY', rnd)
+    let accessory = that.server._publishedAccessories[Object.keys(that.server._publishedAccessories)[0]]
+    let service = accessory.getService(Service.HumiditySensor)
+    let ch = service.getCharacteristic(Characteristic.CurrentRelativeHumidity)
+    ch.getValue((context, value) => {
+      try {
+        expect(value).to.be(rnd)
+        done()
+      } catch (e) {
+        done(e)
+      }
+    })
+  })
+})
