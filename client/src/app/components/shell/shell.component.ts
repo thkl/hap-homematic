f2096896ae5c9e55aaf3c3b80435607505ceda7f
thkl <th.kluge@me.com>
2021-09-03 12:48:00 +0200

    bugfix

--- client/src/app/components/shell/shell.component.ts
+++ client/src/app/components/shell/shell.component.ts
@@ -18,7 +18,7 @@ export class ShellComponent implements OnInit {
     this.store.dispatch({ type: Actions.HapVariableActionTypes.LOAD_VARIABLE });
     this.store.dispatch({ type: Actions.HapInstanceActionTypes.LOAD_INSTANCES });
     this.store.dispatch({ type: Actions.HapSpecialDevicesActionTypes.LOAD_DEVICE });
-
+    this.store.dispatch({ type: Actions.HapRoomActionTypes.LOAD_ROOMS });
   }
 
 }
